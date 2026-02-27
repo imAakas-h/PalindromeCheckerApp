ef57d0bce6ffc749df874a6a689c1fdc837c8fae
imAakas-h <ak71789837@gmail.com>
2026-02-27 09:10:05 +0530

    UC8:Linked List based palindrome checker app

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+}
